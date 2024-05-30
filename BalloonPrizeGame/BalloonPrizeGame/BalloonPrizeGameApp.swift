46b97b17c68847272df02533f7f480837a178214
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-05-30 08:39:04 -0400

    Project Creation

---
+++ BalloonPrizeGame/BalloonPrizeGame/BalloonPrizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonPrizeGameApp.swift
+//  BalloonPrizeGame
+//
+//  Created by Mamadou Balde on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BalloonPrizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
